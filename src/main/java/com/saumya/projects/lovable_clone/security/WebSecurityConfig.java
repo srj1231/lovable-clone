3f42f9089fae3d6e5c107dcf6756532f1cdd9bfe
srj1231 <saumyasrjjha@gmail.com>
2026-07-05 23:53:22 +0530

    (feature):sign up logic implemented

--- src/main/java/com/saumya/projects/lovable_clone/security/WebSecurityConfig.java
+++ src/main/java/com/saumya/projects/lovable_clone/security/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 // overriding spring security default behaviour to allow all requests
@@ -22,5 +24,10 @@ public class WebSecurityConfig {
                 );
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/saumya/projects/lovable_clone/service/impl/AuthServiceImpl.java
+++ src/main/java/com/saumya/projects/lovable_clone/service/impl/AuthServiceImpl.java
@@ -3,15 +3,38 @@ package com.saumya.projects.lovable_clone.service.impl;
 import com.saumya.projects.lovable_clone.dto.auth.AuthResponse;
 import com.saumya.projects.lovable_clone.dto.auth.LoginRequest;
 import com.saumya.projects.lovable_clone.dto.auth.SignupRequest;
+import com.saumya.projects.lovable_clone.entity.User;
+import com.saumya.projects.lovable_clone.exceptions.BadRequestException;
+import com.saumya.projects.lovable_clone.mapper.UserMapper;
+import com.saumya.projects.lovable_clone.repository.UserRepository;
 import com.saumya.projects.lovable_clone.service.AuthService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthServiceImpl implements AuthService {
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+    PasswordEncoder passwordEncoder;
 
     @Override
     public AuthResponse signup(SignupRequest request) {
-        return null;
+        userRepository.findByUsername(request.username()).ifPresent(user -> {
+            throw new BadRequestException("User already exists" + request.username());
+        });
+
+        User user = userMapper.toEntity(request);
+        // this stores password as normal String, use Spring Security
+        user.setPassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+
+        return new AuthResponse("dummy_token", userMapper.toProfileResponse(user));
     }
 
     @Override
